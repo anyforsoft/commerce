56e901dc2edd15fff773ef3e4121dd1d794da783
Bojan Zivanovic <bojanz@gmail.com>
2016-10-03 02:26:28 +0200

    Try without module enable.

--- .travis-before-script.sh
+++ .travis-before-script.sh
@@ -6,8 +6,5 @@ set -e $DRUPAL_TI_DEBUG
 # Note: This function is re-entrant.
 drupal_ti_ensure_drupal
 
-# Enable main module and submodules.
-drush en -y commerce_product commerce_order commerce_checkout commerce_payment
-
 # Turn on PhantomJS for functional Javascript tests
 phantomjs --ssl-protocol=any --ignore-ssl-errors=true $DRUPAL_TI_DRUPAL_DIR/vendor/jcalderonzumba/gastonjs/src/Client/main.js 8510 1024 768 2>&1 >> /dev/null &
